74acbf75336760c3d0c2f4ff7a8c2e853d2cf86a
LucaGusmini <lucag997@gmail.com>
2016-05-17 12:01:07 +0200

    modifica

---
+++ tabellone.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<stdio.h>
+
+
+main() {
+	char r=220,e=219,q=223;
+	int x,c;
+	
+	for(x=0;x<69;x++){
+	printf("%c",r);}
+	
+	printf("\n");
+	
+	for(c=4;c<74;c++){
+	if(c%4==0)
+	printf("%c",e);
+	else
+	printf(" ");}
+	
+	printf("\n");
+	
+	
+		for(c=4;c<74;c++){
+	if(c%4==0)
+	printf("%c",e);
+	else
+	printf(" ");}
+	
+	printf("\n");
+	
+	for(x=0;x<69;x++){
+	printf("%c",q);}
+	
+	printf("\n");
+	
+	//seconda rica
+	for(x=0;x<69;x++){
+	printf("%c",r);}
+	
+	printf("\n");
+	
+	for(c=4;c<74;c++){
+	if(c%4==0)
+	printf("%c",e);
+	else
+	printf(" ");}
+	
+	printf("\n");
+	
+	
+		for(c=4;c<74;c++){
+	if(c%4==0)
+	printf("%c",e);
+	else
+	printf(" ");}
+	
+	printf("\n");
+	
+	for(x=0;x<69;x++){
+	printf("%c",q);}
+	
+	printf("\n");
+	
+	
+	//5 righe finali
+	
+		for(x=0;x<21;x++){
+	printf("-");}
+	
+	printf("\n");
+	
+	for(c=4;c<28;c++){
+	if(c%4==0)
+	printf("|");
+	else
+	printf(" ");}
+	
+	printf("\n");
+	
+	for(x=0;x<21;x++){
+	printf("-");}
+	
+	printf("\n");
+
+
+	
+getch();
+}
+
